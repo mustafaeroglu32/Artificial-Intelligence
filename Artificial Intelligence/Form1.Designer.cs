095e50e428519291daa7a0ee09b97613c523c9b7
mustafaeroglu32 <46123229+mustafaeroglu32@users.noreply.github.com>
2019-10-08 02:25:10 +0300

    Initial commit

---
+++ Artificial Intelligence/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Artificial_Intelligence
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.listBoxDiyalog = new System.Windows.Forms.ListBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.btnMultiSelection = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.tbMessage.Location = new System.Drawing.Point(12, 327);
+            this.tbMessage.MaximumSize = new System.Drawing.Size(297, 80);
+            this.tbMessage.MinimumSize = new System.Drawing.Size(297, 20);
+            this.tbMessage.Multiline = true;
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.Size = new System.Drawing.Size(297, 80);
+            this.tbMessage.TabIndex = 1;
+            this.tbMessage.TextChanged += new System.EventHandler(this.tbMessage_TextChanged);
+            this.tbMessage.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbMessage_KeyDown);
+            // 
+            // listBoxDiyalog
+            // 
+            this.listBoxDiyalog.FormattingEnabled = true;
+            this.listBoxDiyalog.Location = new System.Drawing.Point(12, 12);
+            this.listBoxDiyalog.Name = "listBoxDiyalog";
+            this.listBoxDiyalog.Size = new System.Drawing.Size(297, 251);
+            this.listBoxDiyalog.TabIndex = 2;
+            this.listBoxDiyalog.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(12, 298);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Tümünü Sil";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(12, 269);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Seçileni Sil";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnChange
+            // 
+            this.btnChange.Location = new System.Drawing.Point(93, 269);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(75, 23);
+            this.btnChange.TabIndex = 5;
+            this.btnChange.Text = "Değiştir";
+            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
+            // 
+            // btnMultiSelection
+            // 
+            this.btnMultiSelection.Location = new System.Drawing.Point(234, 269);
+            this.btnMultiSelection.Name = "btnMultiSelection";
+            this.btnMultiSelection.Size = new System.Drawing.Size(75, 23);
+            this.btnMultiSelection.TabIndex = 6;
+            this.btnMultiSelection.Text = "Çoklu Seç";
+            this.btnMultiSelection.UseVisualStyleBackColor = true;
+            this.btnMultiSelection.Click += new System.EventHandler(this.btnMultiSelection_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 424);
+            this.Controls.Add(this.btnMultiSelection);
+            this.Controls.Add(this.btnChange);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.listBoxDiyalog);
+            this.Controls.Add(this.tbMessage);
+            this.Name = "Form1";
+            this.Text = "Artificial Intelligence";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBoxDiyalog;
+        private System.Windows.Forms.TextBox tbMessage;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.Button btnMultiSelection;
+    }
+}
+
---
+++ Artificial Intelligence/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Artificial_Intelligence
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Değişkenler
+        private readonly List<string> MessageSource = new List<string> { "Sen: ", "Bilgisayar: " };
+        private readonly DateTime ZamanEtiketi = DateTime.Now;
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbMessage_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbMessage_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (tbMessage.Text != "")
+                    {
+                    listBoxDiyalog.Items.Add(MessageSource[0] + "[" + ZamanEtiketi + "]: " + tbMessage.Text);
+                    tbMessage.Text = "";
+
+                }
+                
+            }
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            listBoxDiyalog.Items.Clear();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (listBoxDiyalog.SelectedItem == null)
+            {
+                MessageBox.Show("Lütfen bir öğe seçin.");
+            }
+            else
+            {
+                listBoxDiyalog.Items.Remove(listBoxDiyalog.SelectedItem);
+            }
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+           // listBoxDiyalog.selec
+        }
+
+        private void btnMultiSelection_Click(object sender, EventArgs e)
+        {
+            if (listBoxDiyalog.SelectionMode == SelectionMode.One)
+            {
+                listBoxDiyalog.SelectionMode = SelectionMode.MultiSimple;
+                btnMultiSelection.Text = "Tek Seçim";
+            }
+            else if (listBoxDiyalog.SelectionMode == SelectionMode.MultiSimple)
+            {
+                listBoxDiyalog.SelectionMode = SelectionMode.One;
+                btnMultiSelection.Text = "Çoklu Seçim";
+            }
+            
+        }
+    }
+}
